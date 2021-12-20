a9104fa7b60dc15e50ff13dddc4e70cd174510fa
Sooyoung Sun <sysun0813@naver.com>
2021-12-21 01:42:55 +0900

    Update : Block move, roation and map setting
    블록이 자동으로 아래로 내려오는 기능, 화살표에 따라 좌우와 아래로 이동, 회전하는 기능 추가

---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Vector3 RotationPoint;
+    private float previousTime, previousTimeLeft, previousTimeRight;
+    public float FallTime = 0.8f;
+    public static int Height = 20;
+    public static int Width = 10;
+    private static Transform[,] grid = new Transform[Width, Height];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //키보드 입력값이 왼쪽 화살표이면 moveLeft함수, 오른쪽 화살표이면 moveRight함수 실행
+        if((Input.GetKey(KeyCode.LeftArrow) && Time.time - previousTimeLeft > (Input.GetKey(KeyCode.LeftArrow) ? FallTime / 8 : FallTime)))
+        {
+            moveLeft();
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow) && Time.time - previousTimeRight > (Input.GetKey(KeyCode.RightArrow) ? FallTime / 8 : FallTime)))
+        {
+            moveRight();
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rotateBlock();
+        }
+
+
+        if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? FallTime/14 : FallTime))
+        {
+            moveDown();
+        }
+    }
+
+    void moveLeft()
+    {
+        transform.position += new Vector3(-1, 0, 0);//왼쪽으로 이동
+        if(!ValidMove())
+        {
+            transform.position -= new Vector3(-1, 0, 0);
+        }
+        previousTimeLeft = Time.time;
+    }
+
+    void moveRight()
+    {
+        transform.position += new Vector3(1, 0, 0);//오른쪽으로 이동
+        if (!ValidMove())
+        {
+            transform.position -= new Vector3(1, 0, 0);
+        }
+        previousTimeRight = Time.time;
+    }
+
+
+    void moveDown()
+    {
+        transform.position += new Vector3(0, -1, 0);
+        if (!ValidMove())
+        {
+            transform.position -= new Vector3(0, -1, 0);
+        }
+        previousTime = Time.time;
+    }
+
+    void rotateBlock()
+    {
+        transform.RotateAround(transform.TransformPoint(RotationPoint), new Vector3(0, 0, 1), 90);
+        if (!ValidMove())
+        {
+            transform.RotateAround(transform.TransformPoint(RotationPoint), new Vector3(0, 0, 1), -90);
+        }
+    }
+
+
+
+    //맵 범위 내에서 움직이고 있는지 확인하는 함수
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX<0||roundedX>=Width||roundedY<0||roundedY>=Height)
+            {
+                return false;
+            }
+            /*if(grid[roundedX,roundedY]!=null)
+            {
+                return false;
+            }*/
+        }
+        return true;
+    }
+}
