1bbb08137a96fbcb6bc2e7ff4aa18bd08963efa9
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2021-12-21 14:32:36 +0900

    Update TetrisSpawner.cs
    코드 수정

--- Assets/Scripts/TetrisSpawner.cs
+++ Assets/Scripts/TetrisSpawner.cs
@@ -34,7 +34,7 @@ public class TetrisSpawner : MonoBehaviour
 
             //다음 스폰될 테트로미노 보여주기
             nextSpawn = Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position + new Vector3(5.6f, -0.2f, 0), Quaternion.identity);
-            //nextSpawn.GetComponent<TetrisBlock>().enabled = false;
+            nextSpawn.GetComponent<TetrisBlock>().enabled = false;
             nextSpawn.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
         else
