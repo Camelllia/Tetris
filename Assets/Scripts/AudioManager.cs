5791f58b45d4a5d2dacac30ddf4c4598b3e99fe1
ahyeonnn <88013893+ahyeonnn@users.noreply.github.com>
2021-12-21 15:33:50 +0900

    audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,8 @@
-using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
