7202fb1b477068d16b38ff609e8f3c0508179014
Yamna <96425097+Yamnaaa@users.noreply.github.com>
2021-12-21 17:25:21 +0900

    Create : ScoreManager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public int bestScore;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<ScoreManager>();
+                if (instance == null)
+                {
+                    var instanceContainer = new GameObject("ScoreManager");
+                    instance = instanceContainer.AddComponent<ScoreManager>();
+                }
+            }
+            return instance;
+        }
+    }
+    private static ScoreManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
