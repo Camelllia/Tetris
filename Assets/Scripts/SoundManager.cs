69747f2bd2107568377818f0c73c02edf5b2032b
ahyeonnn <88013893+ahyeonnn@users.noreply.github.com>
2021-12-24 15:47:47 +0900

    Update : pause/ resume button

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    public GameObject mana;
+
+    bool isplaying;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isplaying = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Onclick()
+    {
+
+    }
+}
