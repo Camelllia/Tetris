87da61474c5dc2797db4662a95acb15b680bd632
ahyeonnn <88013893+ahyeonnn@users.noreply.github.com>
2021-12-29 20:06:28 +0900

    Update OptionMenu.cs

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class OptionMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-    public GameObject OptionsMenuCanvas;
-}
+        public GameObject optionMenu;
+        public static bool GameIsPaused = false;
+        public GameObject OptionMenuCanvas;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-public void option_btn_clicked()
-{
-    mainMenu.SetActive(false);
-    optionMenu.SetActive(true);
-}
-public void option_back_clicked()
-{
-    optionMenu.SetActive(false);
-    mainMenu.SetActive(true);
-}
+    public void option_btn_clicked()
+    {
+        //mainMenu.SetActive(false);
+        //optionMenu.SetActive(true);
+    }
+    public void option_back_clicked()
+    {
+       // optionMenu.SetActive(false);
+        //mainMenu.SetActive(true);
+    }
 }
